93ef70a144d34000f657aa5d98fa438301eac6b1
DocBlack89 <antoinegibier10@hotmail.fr>
2018-06-02 13:49:16 +0200

    FINIIIIIIIS

--- projet.asm
+++ projet.asm
@@ -17,20 +17,20 @@ includelib c:\masm32\lib\msvcrt.lib
 
 .DATA
 ; variables initialisées
-pathdep 		db "Entrer le chemin du repertoire à lister avec des \ : ",0
-Tab 	db "   ",0
-indicfichier 		db "<fichier> %s",10,0
-indicdossier 		db "<dossier> %s",10,0
-currentFolder 	db ".",0
-previousFolder 	db "..",0
-ajoutslash 		db "/",0
-ajoutfin 		db "/*.*",0
 strCommand 		db "Pause",13,10,0
 format 			db "%s",0
+pathdep 		db "Entrer le chemin du repertoire à lister avec des \ : ",0
+Tab 	db "   ",0
+previousFolder 	db "..",0
+currentFolder 	db ".",0
+indicfichier 		db "<fichier> %s",10,0
+indicdossier 		db "<dossier> %s",10,0
+ajoutslash 		db "/",0
+ajoutfin 		db "/*",0
 
 
 .DATA?
-; variables non-initialisées (bss)
+; variables non-initialisées
 result 	WIN32_FIND_DATA <>
 path 		db 				?
 
@@ -39,9 +39,13 @@ path 		db 				?
 
 
 
-listDirectory PROC
+listDirectory:
 
-    ; préparation de la pile                                 
+    ; préparation de la pile   
+	; ebp = bas de pile                      
+	; esp = haut de pile                                     
+	; on passe le bas de pile en haut de la pile via le push 
+	; puis on déplace le haut de pile en bas de pile  
 	push ebp
 	mov ebp,esp
 	sub esp, 260
@@ -51,8 +55,7 @@ listDirectory PROC
 
 	push ebx
 	push edx
-	call crt_strcpy ; on place dans notre variable locale l'argument Path
-	; restauration de la pile : 2 arguments de 4 octets
+	call crt_strcpy 
 	add esp,8 
 
 	; EDX <- Path local
@@ -61,10 +64,10 @@ listDirectory PROC
 
 	push offset ajoutfin
 	push edx
-	call crt_strcat ; EDX <- Path local + "/*.*"
+	call crt_strcat ; Path local + "/*"
 	add esp,8 
 
-	; EDX <- Path local = Path argument/*.*
+	; EDX <- Path local = Path argument/*
 	mov edx,ebp
 	sub edx,260 						
 
@@ -72,23 +75,21 @@ listDirectory PROC
 	push edx
 	call FindFirstFile ; EAX <- return du FindFirstFile
 
-	; EBX <- récupération du handle
 	mov ebx,ebp
 	sub ebx,4 							
-	; stockage du handle dans ebp-4
 	mov [ebx], eax 						
 
 	doWhile:
-		; verification si File ou Directory
+		; comparaison pour savoir si c'est un dossier
 		cmp result.dwFileAttributes, FILE_ATTRIBUTE_DIRECTORY
 		jne file 
 
 		directory:
-		    ; -- DEBUT gestion des "." et ".." --
+		    ; On passe les "." et les ".."
 				push offset currentFolder 
 				push offset result.cFileName
 				call crt_strcmp
-				; restauration de la pile : 2 arguments de 4 octets
+				; restauration de la pile
 				add esp,8
 				cmp eax,0
 				je nextFile 
@@ -98,7 +99,7 @@ listDirectory PROC
 				add esp,8 					 
 				cmp eax,0
 				je nextFile 
-			; -- FIN gestion des "." et ".." --
+			; fin des "." et ".."
 
 			; ECX <- Path local
 			mov ecx,[ebp+8]
@@ -144,7 +145,6 @@ listDirectory PROC
 
 			push edx
 			push eax
-			; appel recursif avec le nouveau Path (dossier dans le dossier donc) et la tabulation incrémentée
 			call listDirectory
 
 			jmp nextFile
@@ -163,14 +163,13 @@ listDirectory PROC
 
 		nextFile:
 			mov ecx,ebp
-			sub ecx,4 ; stockage du handle
+			sub ecx,4 
 			push offset result 
 			push [ecx]
 			call FindNextFile
 			cmp eax,1 
 			je doWhile 
 
-	; ECX <- handle
 	mov ecx,ebp
 	sub ecx,4 
 	push [ecx]
@@ -179,24 +178,17 @@ listDirectory PROC
 	mov esp,ebp
 	pop ebp
 	ret
-listDirectory ENDP
 
 
-tabulation PROC
+tabulation:
 
     ; préparation de la pile                                 
-	; ebp = bas de pile                      
-	; esp = haut de pile                                     
-	; on passe le bas de pile en haut de la pile via le push 
-	; puis on déplace le haut de pile en bas de pile         
+       
 	push ebp
-	mov ebp,esp ; le haut de pile et le bas de pile pointent au même endroit
-
-	; réservation d'espace pour variable locale pour contenir la variable tabulation
+	mov ebp,esp 
 	sub esp, 4                     
 	; EBX <- tabulation passé en argument
 	mov ebx,[ebp+8]                
-	; si compteur == 0 -> return
 	cmp ebx,0
 	je return                      
 	mov [ebp-4],ebx                ; on place notre compteur dans notre variable locale
@@ -204,21 +196,18 @@ tabulation PROC
 		; utilisation de EAX pour gérer le compteur
 		push offset Tab
 		call crt_printf
-		; restauration de la pile (1 argument de 4 octets)
 		add esp,4                  
 
 		mov eax,[ebp-4]
 		dec eax
 		mov [ebp-4],eax ; on decremente le compteur puis on le replace dans la variable locale
 
-		; si compteur != 0 on boucle
 		cmp eax,0
 		jne loop_inc               
 	return:
 		mov esp,ebp
 		pop ebp
 		ret
-tabulation ENDP
 
 
 start:
