cf0221d79b51b77d7a6ca4f362e9209613a3ef3c
DocBlack89 <antoinegibier10@hotmail.fr>
2018-06-02 13:30:11 +0200

    finis avant modif commentaire

--- make.bat
+++ make.bat
@@ -1,6 +1,4 @@
 @echo off
 c:\masm32\bin\ml /c /Zd /coff projet.asm
 c:\\masm32\bin\Link /SUBSYSTEM:CONSOLE projet.obj
-c:\masm32\bin\ml /c /Zd /coff copi.asm
-c:\\masm32\bin\Link /SUBSYSTEM:CONSOLE copi.obj
 pause
--- projet.asm
+++ projet.asm
@@ -17,11 +17,10 @@ includelib c:\masm32\lib\msvcrt.lib
 
 .DATA
 ; variables initialisées
-pathdep 		db "Entrer le chemin du répertoire à lister avec des \ : ",0
+pathdep 		db "Entrer le chemin du repertoire à lister avec des \ : ",0
 Tab 	db "   ",0
 indicfichier 		db "<fichier> %s",10,0
 indicdossier 		db "<dossier> %s",10,0
-mytestpath 		db ".",0
 currentFolder 	db ".",0
 previousFolder 	db "..",0
 ajoutslash 		db "/",0
@@ -33,65 +32,21 @@ format 			db "%s",0
 .DATA?
 ; variables non-initialisées (bss)
 result 	WIN32_FIND_DATA <>
-pathUser 		db 				?
+path 		db 				?
 
 
 .CODE
-tabulation PROC
 
-    ; préparation de la pile                                 
-	; ebp = bas de pile                      
-	; esp = haut de pile                                     
-	; on passe le bas de pile en haut de la pile via le push 
-	; puis on déplace le haut de pile en bas de pile         
-	push ebp
-	mov ebp,esp ; le haut de pile et le bas de pile pointent au même endroit
-
-	; réservation d'espace pour variable locale pour contenir la variable tabulation
-	sub esp, 4                     
-	; EBX <- tabulation passé en argument
-	mov ebx,[ebp+8]                
-	; si compteur == 0 -> return
-	cmp ebx,0
-	je return                      
-	mov [ebp-4],ebx                ; on place notre compteur dans notre variable locale
-	loop_inc:
-		; utilisation de EAX pour gérer le compteur
-		push offset Tab
-		call crt_printf
-		; restauration de la pile (1 argument de 4 octets)
-		add esp,4                  
-
-		mov eax,[ebp-4]
-		dec eax
-		mov [ebp-4],eax ; on decremente le compteur puis on le replace dans la variable locale
-
-		; si compteur != 0 on boucle
-		cmp eax,0
-		jne loop_inc               
-	return:
-		mov esp,ebp
-		pop ebp
-		ret
-tabulation ENDP
 
 
 listDirectory PROC
 
     ; préparation de la pile                                 
-	; ebp = bas de pile                                      
-	; esp = haut de pile                                     
-	; on passe le bas de pile en haut de la pile via le push 
-	; puis on déplace le haut de pile en bas de pile 
 	push ebp
 	mov ebp,esp
-
-    ;     4 octets pour le HANDLE [ebp+4]
-    ; + 256 octets pour le PATH   [ebp+8]
 	sub esp, 260
 	mov edx,esp
 
-	; EBX <- Path argument
 	mov ebx, [ebp+8] 					
 
 	push ebx
@@ -210,16 +165,15 @@ listDirectory PROC
 			mov ecx,ebp
 			sub ecx,4 ; stockage du handle
 			push offset result 
-			push [ecx] ; FindNextFile demande une string en argument, on push donc la valeur du handle
+			push [ecx]
 			call FindNextFile
-			; retour de FindNextFile : si 0 => pu de fichier, si 1 => continuer
 			cmp eax,1 
 			je doWhile 
 
 	; ECX <- handle
 	mov ecx,ebp
 	sub ecx,4 
-	push [ecx] ; FindClose demande une string en argument, on push donc la valeur du handle
+	push [ecx]
 	call FindClose
 
 	mov esp,ebp
@@ -228,15 +182,54 @@ listDirectory PROC
 listDirectory ENDP
 
 
+tabulation PROC
+
+    ; préparation de la pile                                 
+	; ebp = bas de pile                      
+	; esp = haut de pile                                     
+	; on passe le bas de pile en haut de la pile via le push 
+	; puis on déplace le haut de pile en bas de pile         
+	push ebp
+	mov ebp,esp ; le haut de pile et le bas de pile pointent au même endroit
+
+	; réservation d'espace pour variable locale pour contenir la variable tabulation
+	sub esp, 4                     
+	; EBX <- tabulation passé en argument
+	mov ebx,[ebp+8]                
+	; si compteur == 0 -> return
+	cmp ebx,0
+	je return                      
+	mov [ebp-4],ebx                ; on place notre compteur dans notre variable locale
+	loop_inc:
+		; utilisation de EAX pour gérer le compteur
+		push offset Tab
+		call crt_printf
+		; restauration de la pile (1 argument de 4 octets)
+		add esp,4                  
+
+		mov eax,[ebp-4]
+		dec eax
+		mov [ebp-4],eax ; on decremente le compteur puis on le replace dans la variable locale
+
+		; si compteur != 0 on boucle
+		cmp eax,0
+		jne loop_inc               
+	return:
+		mov esp,ebp
+		pop ebp
+		ret
+tabulation ENDP
+
+
 start:
 	push offset pathdep
 	call crt_printf
-	push offset pathUser
+	push offset path
 	push offset format
 	call crt_scanf 
 
 	push 0
-	push offset pathUser
+	push offset path
 	call listDirectory
 
 	invoke crt_system, offset strCommand
